dd54d4429a0bbcac4a48ada9448067b2445e6a01
VladKappa <vladkappa@protonmail.com>
2020-09-17 15:20:06 +0300

    S-a rezolvat verificarea argv

--- ceitidate.py
+++ ceitidate.py
@@ -1,27 +1,27 @@
 import sys # Pentru command line arguments
-import requests # Pentru a face un request de tip POST
-from bs4 import BeautifulSoup # Ne ajuta preluarea datelor
+import requests # Pentru a face un request de tip POST [ python -m pip install Requests ]
+from bs4 import BeautifulSoup # Ne ajuta preluarea datelor [ python -m pip install BeautifulSoup4 ]
 
-if len(sys.argv[1]) != 13:
-    idnp = int(input('IDNP='))
+if len(sys.argv) > 1:
+	idnp = sys.argv[1]
 else:
-    idnp = sys.argv[1]
+	idnp = int(input('IDNP='))
 
 parametru = {'idnp':idnp}
 
 with requests.session() as s:
-    url = "https://api.ceiti.md/date/login"
-    r = s.post(url, data = parametru)
+	url = "https://api.ceiti.md/date/login"
+	r = s.post(url, data = parametru)
 
-    logat = False if("IDNP format din 13 cifre" in r.text) else True
-    if(not logat):
-        print('IDNP-UL NU A FOST GASIT!')
-        exit()
+	logat = False if("IDNP format din 13 cifre" in r.text) else True
+	if(not logat):
+		print('IDNP-UL NU A FOST GASIT!')
+		exit()
 
-    soup = BeautifulSoup(r.content, 'html.parser')
-    date_personale = soup.find(id='date-personale')
-    data1 = date_personale.find_all('th')
-    data2 = date_personale.find_all('td')
+	soup = BeautifulSoup(r.content, 'html.parser')
+	date_personale = soup.find(id='date-personale')
+	data1 = date_personale.find_all('th')
+	data2 = date_personale.find_all('td')
 
-    for a,b in zip(data1,data2):
-        print(a.get_text() + ': ' + b.get_text())
+	for a,b in zip(data1,data2):
+		print(a.get_text() + ': ' + b.get_text())
